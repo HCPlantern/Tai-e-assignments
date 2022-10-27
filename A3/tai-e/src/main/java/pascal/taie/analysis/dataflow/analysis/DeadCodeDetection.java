80ce75614c368919e80a9accafd62420578243fb
HC_Plantern <1345010966@qq.com>
2022-10-27 17:26:45 +0800

    feat(lab3): finish unreachable code

--- A3/tai-e/src/main/java/pascal/taie/analysis/dataflow/analysis/DeadCodeDetection.java
+++ A3/tai-e/src/main/java/pascal/taie/analysis/dataflow/analysis/DeadCodeDetection.java
@@ -25,14 +25,19 @@ package pascal.taie.analysis.dataflow.analysis;
 import pascal.taie.analysis.MethodAnalysis;
 import pascal.taie.analysis.dataflow.analysis.constprop.CPFact;
 import pascal.taie.analysis.dataflow.analysis.constprop.ConstantPropagation;
+import pascal.taie.analysis.dataflow.analysis.constprop.Value;
 import pascal.taie.analysis.dataflow.fact.DataflowResult;
 import pascal.taie.analysis.dataflow.fact.SetFact;
 import pascal.taie.analysis.graph.cfg.CFG;
 import pascal.taie.analysis.graph.cfg.CFGBuilder;
+import pascal.taie.analysis.graph.cfg.Edge;
 import pascal.taie.config.AnalysisConfig;
 import pascal.taie.ir.IR;
 import pascal.taie.ir.exp.*;
+import pascal.taie.ir.stmt.AssignStmt;
+import pascal.taie.ir.stmt.If;
 import pascal.taie.ir.stmt.Stmt;
+import pascal.taie.ir.stmt.SwitchStmt;
 
 import java.util.*;
 
@@ -58,25 +63,70 @@ public class DeadCodeDetection extends MethodAnalysis {
         Set<Stmt> deadCode = new TreeSet<>(Comparator.comparing(Stmt::getIndex));
 
         // Your task is to recognize dead code in ir and add it to deadCode
+
         // dead variable
         ir.getStmts().forEach(stmt -> {
             SetFact<Var> liveVarsFact = liveVars.getResult(stmt);
-            Optional<LValue> lValue = stmt.getDef();
-            if (lValue.isPresent() && lValue.get() instanceof Var var) {
-                for (RValue rValue : stmt.getUses()) {
-                    // if rvalue has side effect, skip current stmt
-                    if (!hasNoSideEffect(rValue)) {
-                        return;
+            if (stmt instanceof AssignStmt<?, ?> assignStmt) {
+                if (assignStmt.getLValue() instanceof Var var && hasNoSideEffect(assignStmt.getRValue())) {
+                    if (!liveVarsFact.contains(var)) {
+                        deadCode.add(stmt);
                     }
                 }
-                if (!liveVarsFact.contains(var)) {
-                    deadCode.add(stmt);
-                }
+            }
+        });
+        // unreachable stmt
+        LinkedList<Stmt> list = new LinkedList<>();
+        list.add(cfg.getEntry());
+        Set<Stmt> visited = new HashSet<>();
+        while (!list.isEmpty()) {
+            Stmt stmt = list.removeFirst();
+            visited.add(stmt);
+            cfg.getOutEdgesOf(stmt)
+                    .stream()
+                    .filter(edge -> !isUnreachableBranch(edge, constants))
+                    .map(Edge::getTarget)
+                    .forEach(target -> {
+                        if (!visited.contains(target)) {
+                            list.add(target);
+                        }
+                    });
+        }
+        ir.forEach(stmt -> {
+            if (!visited.contains(stmt)) {
+                deadCode.add(stmt);
             }
         });
 
         return deadCode;
     }
+
+    private boolean isUnreachableBranch(Edge<Stmt> edge, DataflowResult<Stmt, CPFact> constants) {
+        Stmt source = edge.getSource();
+        if (source instanceof If ifStmt) {
+            Value value = ConstantPropagation.evaluate(ifStmt.getCondition(), constants.getResult(ifStmt));
+            if (value.isConstant()) {
+                return switch (edge.getKind()) {
+                    case IF_TRUE -> value.getConstant() != 1;
+                    case IF_FALSE -> value.getConstant() != 0;
+                    default -> false;
+                };
+            }
+        } else if (source instanceof SwitchStmt switchStmt) {
+            Value value = ConstantPropagation.evaluate(switchStmt.getVar(), constants.getResult(switchStmt));
+            if (value.isConstant()) {
+                return switch (edge.getKind()) {
+                    case SWITCH_CASE -> edge.getCaseValue() != value.getConstant();
+                    // if any other case is reachable, then the default case is unreachable
+                    case SWITCH_DEFAULT -> switchStmt.getCaseValues()
+                            .stream()
+                            .anyMatch(caseValue -> caseValue == value.getConstant());
+                    default -> false;
+                };
+            }
+        }
+        return false;
+}
 
     /**
      * @return true if given RValue has no side effect, otherwise false.
